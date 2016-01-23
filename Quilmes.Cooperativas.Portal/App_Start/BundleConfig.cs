4d6326614b0abf3a694c2643abc69a670a0e93bf
pablozabo <pablozabo@hotmail.com>
2016-01-23 14:13:18 -0300

    Primeros pasos en definición de layout

--- Quilmes.Cooperativas.Portal/App_Start/BundleConfig.cs
+++ Quilmes.Cooperativas.Portal/App_Start/BundleConfig.cs
@@ -8,24 +8,51 @@ namespace Quilmes.Cooperativas.Portal
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.IgnoreList.Clear();
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            #if !DEBUG
+            BundleTable.EnableOptimizations = true;
+            #endif
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                                             
+            bundles.Add(new StyleBundle("~/Content/css/clientStyles")
+              .Include("~/assets/plugins/bootstrap/css/bootstrap.min.css")
+              .Include("~/assets/css/style.css")
+              .Include("~/assets/css/headers/header-default.css")
+              .Include("~/assets/css/footers/footer-v1.css")
+              .Include("~/assets/plugins/animate.css")
+              .Include("~/assets/plugins/line-icons/line-icons.css")
+              .Include("~/assets/plugins/font-awesome/css/font-awesome.min.css")
+              .Include("~/assets/plugins/fancybox/source/jquery.fancybox.css")
+              .Include("~/assets/plugins/revolution-slider/rs-plugin/css/settings.css")
+              .Include("~/assets/css/theme-colors/default.css")
+              .Include("~/assets/css/theme-skins/dark.css")
+              .Include("~/assets/css/custom.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/basicScripts")
+                .Include("~/assets/plugins/jquery/jquery.min.js")
+                //.Include("~/assets/plugins/jquery/jquery-migrate.min.js")
+                .Include("~/assets/plugins/bootstrap/js/bootstrap.min.js")
+
+                .Include("~/assets/plugins/back-to-top.js")
+                .Include("~/assets/plugins/smoothScroll.js")
+                .Include("~/assets/plugins/jquery.parallax.js")
+                .Include("~/assets/plugins/fancybox/source/jquery.fancybox.pack.js")
+                .Include("~/assets/plugins/owl-carousel/owl-carousel/owl.carousel.js")
+                .Include("~/assets/plugins/revolution-slider/rs-plugin/js/jquery.themepunch.tools.min.js")
+                .Include("~/assets/plugins/revolution-slider/rs-plugin/js/jquery.themepunch.revolution.min.js")
+
+                .Include("~/assets/js/custom.js")
+
+                .Include("~/assets/js/app.js")
+                .Include("~/assets/js/plugins/fancy-box.js")
+                .Include("~/assets/js/plugins/owl-carousel.js")
+                .Include("~/assets/js/plugins/revolution-slider.js")
+                .Include("~/assets/js/plugins/revolution-slider.js")
+                .Include("~/assets/js/plugins/style-switcher.js")                
+                );
+
         }
     }
 }
