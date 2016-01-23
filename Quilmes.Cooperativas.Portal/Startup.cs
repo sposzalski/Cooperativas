d2a135122c36c03f10b893006d50d1641a2add62
pablozabo <pablozabo@hotmail.com>
2016-01-22 23:15:05 -0300

    Project creation

---
+++ Quilmes.Cooperativas.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quilmes.Cooperativas.Portal.Startup))]
+namespace Quilmes.Cooperativas.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
